647e4520119acd3c56cae8d7cc873cb6a7662727
Waterfox <github@waterphox.com>
2023-05-27 23:42:58 -0400

    Semantics This now works in the way I want. As far as a demo to interact with boop.pw, it's done.

--- badger_os/examples/boopnettest.py
+++ badger_os/examples/boopnettest.py
@@ -13,7 +13,7 @@ urlBase = "https://boop.pw"
 # Display Setup
 badger = badger2040.Badger2040()
 badger.led(128)
-badger.set_update_speed(2)
+
 
 # Setup buttons
 btnA = machine.Pin(badger2040.BUTTON_A, machine.Pin.IN, machine.Pin.PULL_DOWN)
@@ -22,14 +22,15 @@ btnC = machine.Pin(badger2040.BUTTON_C, machine.Pin.IN, machine.Pin.PULL_DOWN)
 btnD = machine.Pin(badger2040.BUTTON_DOWN, machine.Pin.IN, machine.Pin.PULL_DOWN)
 btnU = machine.Pin(badger2040.BUTTON_UP, machine.Pin.IN, machine.Pin.PULL_DOWN)
 
-# Approximate center lines for buttons A, B and C - Menu Text - Manu Text Size
+# Approximate center lines for buttons A, B and C - Menu Text - Manu Text Size - change global
 centers = (41, 147, 253)
 btnlabels = ("EXIT", "", "Boop")
 btnlabelsize = 0.8
+change = False
 
 # Connects to the wireless network. Ensure you have entered your details in WIFI_CONFIG.py :)
 badger.connect()
-
+badger.set_update_speed(3)
 
 def getBoop(new = False):
     global boopCount
@@ -64,37 +65,36 @@ def drawPage():
     badger.set_pen(15)
     badger.text("Boop Count", 3, 4)
     badger.set_pen(0)
-
     badger.set_font("bitmap8")
+    badger.text("Loading Boops", int(WIDTH / 4), 28, WIDTH - 105, 4)
+    
+    drawMenu()
+    badger.update()
+    getBoop(change)
+    drawBoop()
 
+def drawBoop():
+    badger.set_font("bitmap8")
+    badger.set_pen(15)
+    badger.rectangle(0, 21, WIDTH, HEIGHT - 50)
+    badger.set_pen(0)
     if boopCount is not None:
         badger.text(boopCount, int(WIDTH / 3), 28, WIDTH - 105, 4)
     else:
         badger.rectangle(0, 60, WIDTH, 25)
         badger.set_pen(15)
         badger.text("Unable to get boops! Check your network settings in WIFI_CONFIG.py", 5, 65, WIDTH, 1)
-    
-    drawMenu()
     badger.update()
 
-getBoop()
 drawPage()
 
 # Button Loop
 while True:
-    changed = False
+    change = False
     if btnC.value():
-        badger.set_pen(15)
-        badger.rectangle(0, 21, WIDTH, HEIGHT - 50)
-        badger.set_pen(0)
-        badger.set_font("bitmap8")
-        badger.text("Reloading", int(WIDTH / 4), 28, WIDTH - 105, 4)
-        badger.update()
-        getBoop(True)
-        changed = True
-    if changed:
+        change = True
         drawPage()
-    if btnA.value():
+    if btnA.value(): # This works best on battery. On USB it just exits the script.
         badger_os.state_clear_running()
         badger2040.reset_pressed_to_wake()
         machine.soft_reset()
